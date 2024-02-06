9255d09b7ea96890ce262dbb9ad218b06f77c5a1
JohnDev0000 <johncroft2236@hotmail.com>
2024-02-06 04:17:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/johnweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.johnweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.johnweb.course.entities.User;
+import com.johnweb.course.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
