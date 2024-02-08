7c8be737ee895d4644082bdd27fadcf04a6952c2
JohnDev0000 <johncroft2236@hotmail.com>
2024-02-08 15:59:09 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/johnweb/course/entities/OrderItem.java
@@ -0,0 +1,77 @@
+package com.johnweb.course.entities;
+
+import com.johnweb.course.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer quantiy;
+    private Double price;
+
+    public OrderItem() {
+
+    }
+
+    public OrderItem(Order order, Product product,Integer quantiy, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantiy = quantiy;
+        this.price = price;
+    }
+
+    public Integer getQuantiy() {
+        return quantiy;
+    }
+
+    public void setQuantiy(Integer quantiy) {
+        this.quantiy = quantiy;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItem orderItem)) return false;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
